f3dee611e729bd3cedf91fc8ade0b53a9a8b5c99
CompiledError <70342656+CompiledError@users.noreply.github.com>
2023-09-12 21:55:04 -0700

    Lab2Checkpoint6

--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -3,6 +3,7 @@ package cs301.birthdaycake;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+    }
+
+    /*public void goodBye() {
+        Log.i("button", "Goodbye");
+    }*/
+
+    public void crashOnPurpose() {
+        Log.e("intentional", "crash");
     }
 }
